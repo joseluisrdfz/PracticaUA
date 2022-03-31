93e7ea2982158a61f8a62da1839fac4848d7f9ee
BernabeSancezSos <bss42@alu.ua.es>
2022-03-31 10:05:38 +0200

    tusmuertos

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Creacion de arrays globales a medida para la seleccion de distintos idiomas
+var chino = [];
+var espanyol = [];
+var ingles = [];
+var hindi = [];
+var arabe = [];
+var portugues = [];
+var ruso = [];
+var urdu = [];
+var bengali = [];
+var frances = [];
+var aleman = [];
+var italiano = [];
 //comprobar si es la primera vez que el usuario usa el horno usando cookies y localstorage
 
 function vaciarCuerpo(){
@@ -23,5 +36,18 @@ function main(x){
 
 
 function pantallaIdiomas(){
-    document.body.innerHTML = '<div> <a>Jaja has mirao</a> </div>';
+    document.body.innerHTML = `<div class ="idiomas" id ="chino"><p>普通话</p></div>
+    <div class = "idiomas" id ="espanyol"><p>Español</p></div>
+    <div class = "idiomas" id ="ingles"><p>English</p></div>
+    <div class = "idiomas" id ="hindi"><p>भारतीय</p></div>
+    <div class = "idiomas" id ="arabe"><p>عرب</p></div>
+    <div class = "idiomas" id ="portugues"><p>Português</p></div>
+    <div class = "idiomas" id ="ruso"><p>pусский</p></div>
+    <div class = "idiomas" id ="urdu"><p>اردو</p></div>
+    <div class = "idiomas" id ="bengali"><p>বাংলা</p></div>
+    <div class = "idiomas" id ="frances"><p>Français</p></div>
+    <div class = "idiomas" id ="aleman"><p>Deutsch</p></div>
+    <div class = "idiomas" id ="italiano"><p>Italiano</p></div>
+    <span class="icon-down-outline arrowdown"></span>
+    <span class="icon-up-outline arrowup"></span>` ;
 }
