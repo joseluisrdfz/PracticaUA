d04cebb4d0eff64f662c7ce33e3887a13715a5ec
BernabeSancezSos <bss42@alu.ua.es>
2022-05-24 22:41:42 +0200

    timeout

--- cliente/interfaz/script.js
+++ cliente/interfaz/script.js
@@ -1,9 +1,7 @@
 var electro = new Electro();
-
 electro.on("connect", function() { // Esparar a que la librería se conecte con la horno
     console.log("Ya estoy conectado con la horno!!")
     console.log("Con este hay " + electro.clientes + " clientes conectados");
-
     // Actualizar el reloj
     /*electro.on("reloj", function (hora) {
         document.getElementById("hora").innerHTML = hora.getHours() + ":" + hora.getMinutes() + ":" + hora.getSeconds();
@@ -21,69 +19,69 @@ electro.on("connect", function() { // Esparar a que la librería se conecte con 
     });
 
     //var cocinar = document.getElementById("cocinar");
-    if (document.getElementById("cocinar") != undefined) var cocinar = document.getElementById("cocinar");
-    console.log(cocinar);
-    var tiempo = document.getElementById("tiempo");
-    var resistenciaSuperior = document.getElementById("resistenciaSuperior");
-    var resistenciaInferior = document.getElementById("resistenciaInferior");
-    var gratinador = document.getElementById("gratinador");
-    var ventilador = document.getElementById("ventilador");
-
-
-    electro.on("puertaAbierta", function(abierta) {
-        cocinar.disabled = abierta;
-    });
-
-    // Cocinar
-    cocinar.addEventListener("click", function() {
-        console.log("Comienzo a cocinar. Tiempo:", tiempo.value);
-        // Bloquear controles
-        cocinar.disabled = true;
-        tiempo.disabled = true;
-        temperatura.disabled = true;
-        resistenciaSuperior.disabled = true;
-        resistenciaInferior.disabled = true;
-        gratinador.disabled = true;
-        ventilador.disabled = true;
-
-        // Ventilador?
-        //if (ventilador.checked) electro.ventilador = true;
-        if (ventiladorActivado == true) electro.ventilador = true;
-
-        // Termostato del horno
-        function termostato(t) {
-            if (t < temperatura.value) { // si no he alcanzado la temperatura objetivo mantener las resistencias activadas
-                if (resistenciaSuperior.checked) electro.resistenciaSuperior = true;
-                if (resistenciaInferior.checked) electro.resistenciaInferior = true;
-                if (gratinador.checked) electro.gratinador = true;
-            } else { // si ya tengo la temperatura objetivo apagar las resistencias
-                electro.resistenciaSuperior = false;
-                electro.resistenciaInferior = false;
-                electro.gratinador = false;
+    setTimeout(function(){
+        var cocinar = document.getElementById("cocinar");
+        /*var tiempo = document.getElementById("tiempo");
+        var resistenciaSuperior = document.getElementById("resistenciaSuperior");
+        var resistenciaInferior = document.getElementById("resistenciaInferior");
+        var gratinador = document.getElementById("gratinador");
+        var ventilador = document.getElementById("ventilador");*/
+    
+    
+        electro.on("puertaAbierta", function(abierta) {
+            cocinar.disabled = abierta;
+        });
+        
+        // Cocinar
+        cocinar.addEventListener("click", function() {
+            //console.log("Comienzo a cocinar. Tiempo:", tiempo.value);
+            // Bloquear controles
+            cocinar.disabled = true;
+            /*tiempo.disabled = true;
+            temperatura.disabled = true;
+            resistenciaSuperior.disabled = true;
+            resistenciaInferior.disabled = true;
+            gratinador.disabled = true;
+            ventilador.disabled = true;*/
+    
+            // Ventilador?
+            //if (ventilador.checked) electro.ventilador = true;
+            if (ventiladorActivado == "true") electro.ventilador = true;
+            // Termostato del horno
+            function termostato(t) {
+                if (t < temperatura.value) { // si no he alcanzado la temperatura objetivo mantener las resistencias activadas
+                    if (resistenciaSuperior.checked) electro.resistenciaSuperior = true;
+                    if (resistenciaInferior.checked) electro.resistenciaInferior = true;
+                    if (gratinador.checked) electro.gratinador = true;
+                } else { // si ya tengo la temperatura objetivo apagar las resistencias
+                    electro.resistenciaSuperior = false;
+                    electro.resistenciaInferior = false;
+                    electro.gratinador = false;
+                }
             }
-        }
-        electro.on("temperaturaInterior", termostato);
-
-        setTimeout(function() {
-            console.log("Fin del cocinado (tiempo cumplido)")
-
-            electro.off("temperaturaInterior", termostato);
-
-            // Desbloquear los controles
-            cocinar.disabled = false;
-            tiempo.disabled = false;
-            temperatura.disabled = false;
-            resistenciaSuperior.disabled = false;
-            resistenciaInferior.disabled = false;
-            gratinador.disabled = false;
-            ventilador.disabled = false;
-
-            // Apagar elementos
-            electro.resistenciaSuperior = electro.resistenciaInferior = electro.gratinador = electro.ventilador = false;
-        }, tiempo.value * 1000);
-    });
+            electro.on("temperaturaInterior", termostato);
+    
+            setTimeout(function() {
+                console.log("Fin del cocinado (tiempo cumplido)")
+    
+                electro.off("temperaturaInterior", termostato);
+    
+                // Desbloquear los controles
+               /* cocinar.disabled = false;
+                tiempo.disabled = false;
+                temperatura.disabled = false;
+                resistenciaSuperior.disabled = false;
+                resistenciaInferior.disabled = false;
+                gratinador.disabled = false;
+                ventilador.disabled = false;*/
+    
+                // Apagar elementos
+                //electro.resistenciaSuperior = electro.resistenciaInferior = electro.gratinador = electro.ventilador = false;
+            }, /*tiempo.value * 1000*/);
+        });
+    },1000 );
+  
 });
-
 
 
 // Creacion de arrays y variables globales a medida para la seleccion de distintos idiomas
